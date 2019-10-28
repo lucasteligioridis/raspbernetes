c1bbfc5d1c0741b287b42b40ace6af7084a626e9
Lucas Teligioridis <lucas.teligioridis@gmail.com>
2019-10-28 23:15:59 +1100

    Modify string into an array for proper splitting

--- scripts/install/kubernetes.sh
+++ scripts/install/kubernetes.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 kube_version="1.16.1-00"
-kube_packages="kubelet=${kube_version} kubectl=${kube_version} kubeadm=${kube_version}"
+kube_packages=("kubelet=${kube_version}" "kubectl=${kube_version}" "kubeadm=${kube_version}")
 
 echo "Disabling swap and ensuring it doesn't turn back on after reboot"
 dphys-swapfile swapoff
@@ -24,8 +24,8 @@ EOF
 # unsure if this has to do with too many concurrent hits from the same public IP
 echo "Installing kubernetes ${kube_version}..."
 until apt-get update; do echo "Retrying to update apt mirrors"; done
-apt-get install -y --no-install-recommends ${kube_packages}
-apt-mark hold ${kube_packages}
+apt-get install -y --no-install-recommends "${kube_packages[@]}"
+apt-mark hold "${kube_packages[@]}"
 
 echo "Setting to use legacy iptables for compatibility issues"
 update-alternatives --set iptables /usr/sbin/iptables-legacy
